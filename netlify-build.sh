401cab9a92f76905008776f71e36743a66124632
lawrns <laurence@fyves.com>
2025-03-16 15:54:57 -0600

    1
    1

--- netlify-build.sh
+++ netlify-build.sh
@@ -17,6 +17,7 @@ echo "🔄 Installing specific dependencies..."
 npm install ajv@8.12.0 --save --legacy-peer-deps
 npm install ajv-keywords@5.1.0 --save --legacy-peer-deps
 npm install react-refresh@0.14.0 --save --legacy-peer-deps
+npm install eslint-webpack-plugin@4.0.1 --save-dev --legacy-peer-deps
 
 echo "🛠️ Setting environment variables..."
 export SKIP_PREFLIGHT_CHECK=true
